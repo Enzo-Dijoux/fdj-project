34b7724e5364e9555a94aa70cd33ba09fa62b714
Enzo-Dijoux <e.dijoux.dvlpt@icloud.com>
2023-10-11 11:32:48 +0200

    Initial Commit

---
+++ FDJ/FDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJApp.swift
+//  FDJ
+//
+//  Created by Enzo Dijoux on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
