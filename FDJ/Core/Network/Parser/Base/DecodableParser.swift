f5f056bf33d500f5a66ef5900e4b367cd383eddc
Enzo-Dijoux <e.dijoux.dvlpt@icloud.com>
2023-10-11 15:01:28 +0200

    Implementation of BaseService and parsing

---
+++ FDJ/Core/Network/Parser/Base/DecodableParser.swift
@@ -0,0 +1,20 @@
+//
+//  DecodableParser.swift
+//  FDJ
+//
+//  Created by Enzo Dijoux on 11/10/2023.
+//
+
+import Foundation
+
+class DecodableParser<T: Decodable> {
+
+    func parse(withResponse data: NSData, headers: [AnyHashable : Any]) -> Any? {
+        do {
+            return try JSONDecoder().decode(T.self, from: data as Data)
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
